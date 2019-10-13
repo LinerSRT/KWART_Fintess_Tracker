668e295d6f852d34cdc50bcacf58f4e0c75dba59
Line'R <serinity320@gmail.com>
2019-10-13 21:58:59 +0300

    Added accel sensor implementation, added ldpi layout

--- app/src/main/java/com/kwart/tracking/utils/workout/WorkoutManager.java
+++ app/src/main/java/com/kwart/tracking/utils/workout/WorkoutManager.java
@@ -69,6 +69,16 @@ public class WorkoutManager implements LocationListener, SensorEventListener, Gp
     private List<Float> speedArray;
     private int stepPerSec, stepCount;
 
+    //Accel step
+    private float   mLimit;
+    private float[] mLastValues = new float[3 * 2];
+    private float[] mScale = new float[2];
+    private float   mYOffset;
+    private float[] mLastDirections = new float[3 * 2];
+    private float[][] mLastExtremes = {new float[3 * 2], new float[3 * 2]};
+    private float[] mLastDiff = new float[3 * 2];
+    private int     mLastMatch = -1;
+
     //User part
     private float userStepSize;
     private int userWeight;
@@ -110,6 +120,12 @@ public class WorkoutManager implements LocationListener, SensorEventListener, Gp
             stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
         } else {
             stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            this.mLimit = 12;
+            int h = 480;
+            mYOffset = h * 0.5f;
+            mScale[0] = -(h * 0.5f * (1.0f / (SensorManager.STANDARD_GRAVITY * 2)));
+            mScale[1] = -(h * 0.5f * (1.0f / (SensorManager.MAGNETIC_FIELD_EARTH_MAX)));
+
         }
         heartRateSensor = sensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
     }
@@ -302,12 +318,55 @@ public class WorkoutManager implements LocationListener, SensorEventListener, Gp
                 }
             }
         if(sensorEvent.sensor == stepSensor){
-            stepCount++;
-            stepPerSec++;
-            workoutItem.setStepCount(stepCount);
-            calculateCalories(stepCount);
-            if(!gpsManagerEnabled)
-                calculateDistanceBySensor(stepCount);
+            if(havePerometer) {
+                stepCount++;
+                stepPerSec++;
+                workoutItem.setStepCount(stepCount);
+                calculateCalories(stepCount);
+                if (!gpsManagerEnabled)
+                    calculateDistanceBySensor(stepCount);
+            } else {
+                synchronized (this) {
+                    int j = (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) ? 1 : 0;
+                    if (j == 1) {
+                        float vSum = 0;
+                        for (int i = 0; i < 3; i++) {
+                            final float v = mYOffset + sensorEvent.values[i] * mScale[j];
+                            vSum += v;
+                        }
+                        int k = 0;
+                        float v = vSum / 3;
+
+                        float direction = (Float.compare(v, mLastValues[k]));
+                        if (direction == -mLastDirections[k]) {
+                            int extType = (direction > 0 ? 0 : 1);
+                            mLastExtremes[extType][k] = mLastValues[k];
+                            float diff = Math.abs(mLastExtremes[extType][k] - mLastExtremes[1 - extType][k]);
+                            if (diff > mLimit) {
+                                boolean isAlmostAsLargeAsPrevious = diff > (mLastDiff[k] * 2 / 3);
+                                boolean isPreviousLargeEnough = mLastDiff[k] > (diff / 3);
+                                boolean isNotContra = (mLastMatch != 1 - extType);
+                                if (isAlmostAsLargeAsPrevious && isPreviousLargeEnough && isNotContra) {
+                                    stepCount++;
+                                    stepPerSec++;
+                                    workoutItem.setStepCount(stepCount);
+                                    calculateCalories(stepCount);
+                                    if (!gpsManagerEnabled)
+                                        calculateDistanceBySensor(stepCount);
+                                    mLastMatch = extType;
+                                } else {
+                                    mLastMatch = -1;
+                                }
+                            }
+                            mLastDiff[k] = diff;
+                        }
+                        mLastDirections[k] = direction;
+                        mLastValues[k] = v;
+                    }
+
+
+                }
+            }
         }
     }
 
